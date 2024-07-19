664320fc9721fc85b6948616f9db8591731f44f0
IvanSaavedra <ivansaavedra@appnet.cl>
2024-07-19 18:28:09 -0400

    cambios a ultima hora

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -147,7 +147,7 @@
     </script>
 
     <script src="{{asset('QR/qrcode.js')}}"></script>
-    <script src="{{asset('totem/script.js?ver=1.1.5')}}"></script>
+    <script src="{{asset('totem/script.js?ver=1.1.6')}}"></script>
 
   </body>
 
