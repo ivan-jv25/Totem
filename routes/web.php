170a95242791e556029c26f7883e64d16b1a33e8
IvanSaavedra <ivansaavedra@appnet.cl>
2024-11-13 13:23:34 -0300

    ruta para limpiar el proyecto

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    
     return view('welcome');
+});
+
+Route::get('/optimize-clear', function () {
+    Artisan::call('optimize:clear');
+    return "Optimización y caché limpiados";
 });
 
 // Route::get('/dashboard', function () { return view('dashboard'); })->middleware(['auth'])->name('dashboard');
